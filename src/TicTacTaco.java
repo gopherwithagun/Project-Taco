9a1960859245a66823f6c418c6eb2c7fff308629
gopherwithagun <ndurwood@gmail.com>
2014-09-07 17:04:00 -0700

    Potato
    Poato Batman

---
+++ src/TicTacTaco.java
@@ -0,0 +1,10 @@
+public class TicTacTaco
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Goodbye World");
+		System.out.println("Make a change right now nope nope nope");
+	}
+
+}
